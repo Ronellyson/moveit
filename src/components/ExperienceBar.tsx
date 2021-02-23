cfc9ff8dd8fa278c9ae02dec2f14a0b52afb6d1a
Ronellyson <ronellysonjulio@gmail.com>
2021-02-23 06:47:07 -0300

    código ao final da primeira aula da Trilha React NLW4

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '40%'}} />
+
+        <span className="current-experience" style={{ left: '40%' }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
